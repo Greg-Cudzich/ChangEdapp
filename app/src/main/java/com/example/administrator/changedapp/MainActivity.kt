e046cd1a318c6b9d160ad32b25681bbdc583f468
Greg <gregcudzich@gmail.com>
2018-04-16 19:53:09 -0500

    reverted back to original state

--- app/src/main/java/com/example/administrator/changedapp/MainActivity.kt
+++ app/src/main/java/com/example/administrator/changedapp/MainActivity.kt
@@ -3,85 +3,26 @@ package com.example.administrator.changedapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TextInputLayout
 import android.support.design.widget.TextInputEditText
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.regex.Pattern
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.tasks.Task
-import android.app.PendingIntent
-import com.google.android.gms.common.api.GoogleApiClient
-import android.widget.TextView
-import com.google.android.gms.common.api.Scope
-import com.sun.org.apache.xpath.internal.operations.Plus
 
 
 
+    class MainActivity : AppCompatActivity(){
 
 
-
-
-
-
-
-     class MainActivity : AppCompatActivity(), ConnectionCallbacks, OnConnectionFailedListener, View.OnClickListener{
-
-         private val SIGNED_IN = 0
-         private val STATE_SIGNING_IN = 1
-         private val STATE_IN_PROGRESS = 2
-         private val RC_SIGN_IN = 0
-         private val mGoogleApiClient: GoogleApiClient? = null
-         private val mSignInProgress: Int = 0
-         private val mSignInIntent: PendingIntent? = null
-
-         private val mSignInButton: SignInButton? = null
-         private val mStatus: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mSignInButton = findViewById(R.id.google_button) as SignInButton
 
-        mSignInButton.setOnClickListener(this)
-        mGoogleApiClient = buildGoogleApiClient()
 
     }
 
-         private fun buildGoogleApiClient(): GoogleApiClient {
-             return GoogleApiClient.Builder(this)
-                     .addConnectionCallbacks(this)
-                     .addOnConnectionFailedListener(this)
-                     .addApi(Plus.API, Plus.PlusOptions.builder().build())
-                     .addScope(Scope("email"))
-                     .build()
-         }
 
-         override fun onStart() {
-             super.onStart()
-             if (mGoogleApiClient != null) {
-                 mGoogleApiClient.connect()
-             }
-         }
-
-         override fun onStop() {
-             super.onStop()
-             if (mGoogleApiClient != null) {
-                 mGoogleApiClient.disconnect()
-             }
-         }
-
-         override fun onConnectionSuspended(cause: Int) {
-             mGoogleApiClient.connect()
-         }
 
 
 
